2cc2dab74e50430357ea343c4b3fe3352ade8be7
Zhou Nan <zhounan.dev@gmail.com>
2019-03-24 18:22:37 +0800

    fix(doc): deploy config

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 生成静态文件
+npm run docs:build
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-npm run docs:build
 git init
 git add -A
 git push -f git@github.com:ZNspace/LeetCode.git.git master:gh-pages
